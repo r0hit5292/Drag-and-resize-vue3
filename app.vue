1cadacf4e39ce5503ea57e50d19f61c8b1fbd836
Rohit Sachan <rohit.sachan@vidyamantra.com>
2023-01-19 13:54:51 +0530

    vue drag and resize with nuxt3

--- app.vue
+++ app.vue
@@ -1,9 +1,39 @@
-<script setup lang="ts">
-    import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue';
-</script >
-
 <template>
-    <VueDragResize :w="200"  :h="120" >
-        Hello World
-    </VueDragResize>
+    <div id="app">
+        <VueDragResize :isActive="true" :w="200" :h="200" v-on:resizing="resize" v-on:dragging="resize">
+           
+            <p>{{ top }} х {{ left }} </p>
+            <p>{{ width }} х {{ height }}</p>
+        </VueDragResize>
+    </div>
 </template>
+
+<script lang="ts">
+    import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue';
+
+    export default {
+        name: 'app',
+
+        components: {
+            VueDragResize
+        },
+
+        data() {
+            return {
+                width: 0,
+                height: 0,
+                top: 0,
+                left: 0
+            }
+        },
+
+        methods: {
+            resize(newRect:any) {
+                this.width = newRect.width;
+                this.height = newRect.height;
+                this.top = newRect.top;
+                this.left = newRect.left;
+            }
+        }
+    }
+</script>
