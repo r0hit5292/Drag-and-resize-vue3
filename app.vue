0466855f79aa1604e019d92de5d7e5298921ff74
Rohit Sachan <rohit.sachan@vidyamantra.com>
2023-01-19 13:42:13 +0530

    vue drag and resize with nuxt3

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue';
+</script >
+
+<template>
+    <VueDragResize :w="200"  :h="120" >
+        Hello World
+    </VueDragResize>
+</template>
